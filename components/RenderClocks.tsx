2af1c2364b8c11c0299b8025994709df3d5a5296
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2021-07-21 03:22:12 -0700

    finished full functionality

--- components/RenderClocks.tsx
+++ components/RenderClocks.tsx
@@ -14,13 +14,28 @@ import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
 import React, { useRef } from "react";
 import Clock from "react-live-clock";
-import { useUpdate } from "react-use";
+import { useLocalStorage, useUpdate } from "react-use";
 import { useState } from "react";
 import getSliderValue from "../helpers/getSliderValue";
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
+// function deleteTime() {
+
+// }
+
 export default function RenderClocks({ arr }) {
+  // const myStorage = localStorage;
+  // localStorage.setItem("countriesArr", "arr[0].countryName");
+  // localStorage;
+  // useLocalStorage
+  // localStorage.
+  // localStorage.clear();
+  // const dataaa = JSON.stringify(arr);
+  // localStorage[1,2,3,4,5] = JSON.stringify(arr);
+  // localStorage["selectedCitiesArray"] = JSON.stringify([arr]);
+  // console.log(wow);
+
   const [delta, setDelta] = useState(null);
   const slider = useRef();
   const update = useUpdate();
@@ -31,15 +46,18 @@ export default function RenderClocks({ arr }) {
 
     return (
       <Box w="100%" key={i} alignItems="center" mt="2.25%">
-        <Flex alignItems="center">
-          <Heading mb="5" pl="2.5%" mt="1%" fontSize="200%" fontWeight="600">
-            {country.city
-              ? `${country.city}`
-              : // `${country.countryName}` `${country.countryCode}`
-                "Your current time"}
+        <Flex alignItems="center" gridGap="1%" pb="1%">
+          <Heading
+            fontSize={["95%", "100%", "125%", "150%", "160%", "175%"]}
+            fontWeight="600"
+          >
+            {country.city ? `${country.city}` : "Your current time"}
             {country.nextAbbreviation && `(${country.nextAbbreviation})`}:{" "}
           </Heading>
-          <Heading fontWeight="300" margin="1%">
+          <Heading
+            fontWeight="300"
+            fontSize={["100%", "130%", "145%", "160%", "170%", "175%"]}
+          >
             {delta !== null ? (
               <Clock
                 date={`${datetime
@@ -59,43 +77,64 @@ export default function RenderClocks({ arr }) {
               />
             )}
           </Heading>
+          {
+            i === 0 ? (
+              <Button
+                ml="auto"
+                // fontSize="140%"
+                fontSize={["100%", "110%", "120%", "130%", "140%", "150%"]}
+                size="xl"
+                p="1%"
+                _focus={{ outline: "none" }}
+                onClick={() => {
+                  setDelta(null);
+                }}
+              >
+                Get Current Times
+              </Button>
+            ) : (
+              <Button
+                ml="auto"
+                // fontSize="140%"
+                fontSize={["100%", "110%", "120%", "130%", "140%", "150%"]}
+                size="xl"
+                p="1%"
+                _focus={{ outline: "none" }}
+                onClick={() => {
+                  arr.splice(i, 1);
+                }}
+              >
+                Delete Time
+              </Button>
+            )
+            // FIXME:FIXME:FIXME:FIXME:FIXME:
+          }
         </Flex>
-        <Flex>
-          <Slider
-            aria-label="slider-ex-4"
-            value={getSliderValue(datetime, delta)}
-            max={24}
-            min={0}
-            step={0.16666667}
-            // step={1 / 60}
-            ref={slider}
-            mr="5%"
-            mb="1%"
-            onChange={(e) => {
-              setDelta(e - datetime.hour());
-            }}
-          >
-            <SliderTrack bg="red">
-              <SliderFilledTrack bg="blue" />
-            </SliderTrack>
-            <SliderThumb boxSize={10} bg="black">
-              <Box
-                color="tomato"
-                //  as={}
-              />
-            </SliderThumb>
-          </Slider>
-          <Button
-            fontSize="150%"
-            size="xl"
-            p="1%"
-            onClick={() => {
-              setDelta(null);
-            }}
-          >
-            Get Current Times
-          </Button>
-        </Flex>
+
+        <Slider
+          aria-label="slider-ex-4"
+          value={getSliderValue(datetime, delta)}
+          max={24}
+          min={0}
+          step={0.16666667}
+          // step={1 / 60}
+          ref={slider}
+          mr="5%"
+          mb="1%"
+          onChange={(e) => {
+            setDelta(e - datetime.hour());
+          }}
+        >
+          <SliderTrack bg="red">
+            <SliderFilledTrack bg="blue" />
+          </SliderTrack>
+          <SliderThumb boxSize={[5, 6, 7, 8, 9, 10]} bg="black">
+            <Box
+              color="tomato"
+              //  as={}
+            />
+          </SliderThumb>
+        </Slider>
 
         {/* <Center fontSize="175%"> */}
         {/* {delta !== null ? (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,15 @@
 import {
+  Text,
   Box,
   Button,
   Center,
   Flex,
+  Heading,
   Input,
-  List,
-  ListItem,
-  Slider,
-  SliderFilledTrack,
-  SliderThumb,
-  SliderTrack,
-  useColorModeValue,
+  FormControl,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
 } from "@chakra-ui/react";
 import React, { useEffect, useRef, useState } from "react";
 import { useAsync, useAsyncFn } from "react-use";
@@ -23,16 +22,22 @@ import usePlacesAutocomplete, {
   getLatLng,
 } from "use-places-autocomplete";
 import useOnclickOutside from "react-cool-onclickoutside";
+import { FocusScope, useFocusManager } from "@react-aria/focus";
+import { IoIosPin, IoIosSearch } from "react-icons/io";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
 const IndexPage = () => {
+  const firstSuggestion = useRef();
+  const search = useRef();
   const clickMe = useRef();
   const [clockStack, setClockStack] = useState([
     { countryName: "Your current time", zoneName: dayjs.tz.guess() },
   ]);
-
+  // const arr = ["1", "2", "3", "4", "5"];
+  // console.log(arr);
+  // FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:BEGINNING OF AUTOPALCESFIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:
   const PlacesAutocomplete = () => {
     const {
       ready,
@@ -59,7 +64,7 @@ const IndexPage = () => {
       setValue(e.target.value);
     };
 
-    const handleSelect = async ({ place_id, description, types }) => {
+    const handleSelect = async ({ place_id, description }) => {
       // When user selects a place, we can replace the keyword without request data from API
       // by setting the second parameter to "false"
       setValue(description, false);
@@ -68,10 +73,8 @@ const IndexPage = () => {
       const results = await getGeocode({
         placeId: place_id,
       });
-
       const city = results[0].address_components[0].long_name;
       const { lat, lng } = await getLatLng(results[0]);
-
       return getTimezone(lat, lng, city);
     };
 
@@ -79,64 +82,142 @@ const IndexPage = () => {
       return data.map((suggestion, i) => {
         const {
           place_id,
-
           structured_formatting: { main_text, secondary_text },
         } = suggestion;
-
         return (
-          <ListItem
-            // id={`${i}`}
-            ref={clickMe}
+          <Box
+            p="0"
+            w="100%"
+            onClick={() => {
+              handleSelect(suggestion);
+            }}
             key={place_id}
-            onClick={() => handleSelect(suggestion)}
-            listStyleType="none"
-            _hover={{ bg: "#DFDFDF" }}
-            fontSize="115%"
-            p="1%"
+            onKeyDown={(e) => {
+              if (e.key === "Enter") handleSelect(suggestion);
+            }}
+            ref={i === 0 ? firstSuggestion : undefined}
           >
-            <strong>{main_text}</strong> <small>{secondary_text}</small>
-          </ListItem>
+            <SearchOptions suggest={suggestion}>
+              <IoIosPin size="25px" />
+              <Heading fontSize="120%" mr="5px">
+                {main_text}
+              </Heading>{" "}
+              <Text color="gray">{secondary_text}</Text>
+            </SearchOptions>
+          </Box>
         );
       });
     };
+    function SearchOptionsContainer(props) {
+      return (
+        <Box role="toolbar" w="100%" p="0">
+          <FocusScope>{props.children}</FocusScope>
+        </Box>
+      );
+    }
+
+    function SearchOptions(props, suggest) {
+      let focusManager = useFocusManager();
+      let onKeyDown = (e) => {
+        switch (e.key) {
+          case "ArrowDown":
+            focusManager.focusNext({
+              wrap: true,
+            });
+            break;
+          case "ArrowUp":
+            focusManager.focusPrevious({ wrap: true });
+            break;
+          case "Enter":
+            handleSelect(suggest);
+        }
+      };
+
+      return (
+        <Button
+          p="0%"
+          w="100%"
+          borderRadius="0"
+          bg="transparent"
+          _hover={{ bg: "#DFDFDF" }}
+          justifyContent="left"
+          _focus={{ bg: "#DFDFDF" }}
+          onKeyDown={onKeyDown}
+          onKeyDownCapture={(e) => {
+            if (e.key === "Escape") clearSuggestions();
+          }}
+        >
+          {props.children}
+        </Button>
+      );
+    }
 
     return (
-      <Flex ref={ref} w="95%" ml="2.5%">
-        <Box w="100%">
-          <Input
-            value={value}
-            onChange={handleInput}
-            disabled={!ready}
-            placeholder="Search for a city or country"
-            pos="sticky"
-            top="0"
-            fontSize="125%"
-            onKeyDown={(e) => {
-              // console.log(e);
-              // console.log(cursor);
-              console.log(data[0]);
+      <Flex ref={ref} w="80%">
+        <Box w="50%">
+          <Flex
+            border="1px solid black"
+            borderRadius="10px"
+            _focusWithin={{ boxShadow: "1px 1px 1px gray" }}
+          >
+            <InputGroup>
+              <Input
+                variant="outline"
+                ref={search}
+                value={value}
+                onChange={handleInput}
+                disabled={!ready}
+                placeholder="Search for a city or country"
+                fontSize="125%"
+                size="lg"
+                border="none"
+                _focus={{ outline: "none" }}
+                onKeyDown={(e) => {
+                  if (data.length > 0 && e.key === "ArrowDown")
+                    // @ts-expect-error
+                    firstSuggestion.current.children[0].focus();
+                }}
+              />
+              <InputRightElement
+                h="100%"
+                children={
+                  <IoIosSearch
+                    size="90%"
+                    // size={["80px", "85px", "90px", "95px", "100px"]}
+                    // @ts-expect-error
+                    onClick={() => search.current?.focus()}
+                  />
+                }
+              />
+            </InputGroup>
+          </Flex>
 
-              if (data.length > 0 && e.key == "a") console.dir(clickMe.current);
-            }}
-          />
           {/* We can use the "status" to decide whether we should display the dropdown or not */}
           {status === "OK" && (
-            <Box
-              p="1%"
-              mt="1%"
+            <Flex
+              p="5px"
+              // mt="1%"
               borderBottomRadius="10px"
-              bg="#EFEFEF"
+              // bg="red"
+              bg="white"
+              pos="fixed"
+              w="20%"
+              mt="5px"
+              zIndex="15"
               boxShadow="0px 2px 5px gray"
             >
-              {" "}
-              <List>{renderSuggestions()}</List>
-            </Box>
+              <SearchOptionsContainer>
+                {renderSuggestions()}
+              </SearchOptionsContainer>
+            </Flex>
           )}
         </Box>
-        <Button onClick={() => console.log("hi")}>Click me!</Button>
+
+        {/* <Button onClick={() => handleSelect(suggestion)}>Click me!</Button> */}
       </Flex>
     );
   };
+  // FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:END OF AUTOPALCESFIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:
 
   const [, getTimezone] = useAsyncFn(async (lat, lng, city) => {
     const response = await fetch(
@@ -157,78 +238,13 @@ const IndexPage = () => {
   }, []);
 
   return (
-    <Flex>
-      <Box w="25%" mt={10} pos="-webkit-sticky">
+    <Center>
+      <Box flexDir="column" w="75%" mt="2%" mr="2.5%" maxH="100%" minH="90vh">
         <PlacesAutocomplete />
-      </Box>
-      <Center
-        flexDir="column"
-        w="100%"
-        mt="2%"
-        mr="2.5%"
-        maxH="100%"
-        minH="100vh"
-        // bg="red.100"
-      >
         <RenderClocks arr={clockStack} />
-      </Center>
-    </Flex>
+      </Box>
+    </Center>
   );
 };
 
 export default IndexPage;
-
-// FIXME:FIXME:FIXME:FIXME:FIXME:
-
-// const Item = ({ character, focus, index, setFocus }) => {
-//   const ref = useRef(null);
-
-//   useEffect(() => {
-//     if (focus) {
-//       // Move element into view when it is focused
-//       ref.current.focus();
-//     }
-//   }, [focus]);
-
-//   const handleSelect = useCallback(() => {
-//     alert(`${character}`);
-//     // setting focus to that element when it is selected
-//     setFocus(index);
-//   }, [character, index, setFocus]);
-
-//   return (
-//     <li
-//       tabIndex={focus ? 0 : -1}
-//       role="button"
-//       ref={ref}
-//       onClick={handleSelect}
-//       onKeyPress={handleSelect}
-//     >
-//       {character}
-//     </li>
-//   );
-// };
-
-function Toolbar(props) {
-  return (
-    <div role="toolbar">
-      <FocusScope>{props.children}</FocusScope>
-    </div>
-  );
-}
-
-function ToolbarButton(props) {
-  let focusManager = useFocusManager();
-  let onKeyDown = (e) => {
-    switch (e.key) {
-      case "ArrowRight":
-        focusManager.focusNext({ wrap: true });
-        break;
-      case "ArrowLeft":
-        focusManager.focusPrevious({ wrap: true });
-        break;
-    }
-  };
-
-  return <button onKeyDown={onKeyDown}>{props.children}</button>;
-}
