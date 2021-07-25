4976330b6733f8768ebf6f4dff2448a6b8d69a9f
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2021-07-25 15:26:19 -0700

    added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,31 @@
 import { ChakraProvider, theme } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import { useEffect } from "react";
+import ReactGA from "react-ga";
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <ChakraProvider theme={theme}>
-    <Component {...pageProps} />
-  </ChakraProvider>
-);
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const logPageView = () => {
+    if (process.env.NODE_ENV === "production") {
+      ReactGA.pageview(pageProps.router.asPath);
+    }
+  };
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === "production") {
+      ReactGA.initialize("UA-203142465-1");
+      logPageView();
+    }
+  }, []);
+
+  useEffect(() => {
+    logPageView();
+  }, [pageProps, Component]);
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+};
 
 export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,15 +18,6 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
           <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-NL32CEP9B6"
-          ></script>
-          <script>
-            window.dataLayer = window.dataLayer || []; function gtag()
-            {dataLayer.push(arguments)}
-            gtag('js', new Date()); gtag('config', 'G-NL32CEP9B6');
-          </script>
-          <script
             src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}&libraries=places`}
           ></script>
         </body>
