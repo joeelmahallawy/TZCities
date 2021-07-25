e7f157b2acf8d2e652a901239aee336420ad89eb
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2021-07-25 00:34:39 -0700

    -

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { IoIosClock } from "react-icons/io";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -9,10 +10,22 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head></Head>
+        <Head>
+          <title>TZ Get</title>
+          <link rel="icon" type="image/png" href={`${IoIosClock}`}></link>
+        </Head>
         <body>
           <Main />
           <NextScript />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-NL32CEP9B6"
+          ></script>
+          <script>
+            window.dataLayer = window.dataLayer || []; function gtag()
+            {dataLayer.push(arguments)}
+            gtag('js', new Date()); gtag('config', 'G-NL32CEP9B6');
+          </script>
           <script
             src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}&libraries=places`}
           ></script>
