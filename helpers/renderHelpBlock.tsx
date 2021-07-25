16ec911845b15eed5aeabcda57dbaff0c28031f7
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2021-07-24 22:43:15 -0700

    removed test api key and fixed invalidkey error

--- helpers/renderHelpBlock.tsx
+++ helpers/renderHelpBlock.tsx
@@ -14,10 +14,8 @@ import {
   Button,
   PopoverArrow,
 } from "@chakra-ui/react";
-
 import React from "react";
 import { IoIosHelpCircle } from "react-icons/io";
-console.log(process.env.NEXT_PUBLIC_GOOGLE_API_KEY);
 export default function RenderHelpBlock() {
   const initRef = React.useRef();
   return (
